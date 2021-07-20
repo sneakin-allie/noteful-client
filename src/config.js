7f0643e3bd1f4522a66db3648346d0945b97b4a7
Allison Schulman <allison.d.schulman@gmail.com>
2021-07-20 15:20:13 -0400

    Changed API BASE URL to Heroku

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
     PORT: process.env.PORT || 8080,
-    API_BASE_URL: `http://localhost:8000/api`,
-    // API_BASE_URL: process.env.REACT_APP_API_BASE_URL || `https://fathomless-plateau-60580.herokuapp.com`,
+    // API_BASE_URL: `http://localhost:8000/api`,
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || `https://fathomless-plateau-60580.herokuapp.com`,
     API_KEY: process.env.REACT_APP_API_KEY,
 }
 
